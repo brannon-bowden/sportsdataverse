a7cad2b015a3276d2e774199741ad2debbd2a212
Brannon <410768+brannon-bowden@users.noreply.github.com>
2021-11-20 08:21:19 -0500

    fix

--- app/services/cfb.service.js
+++ app/services/cfb.service.js
@@ -335,6 +335,19 @@ module.exports = {
 
         return res.content.data;
     },
+
+    getBownSchedule: async function ({ year = null, month = null, day = null, group = 80, seasontype = 2 }) {
+        const baseUrl = `http://cdn.espn.com/college-football/schedule/_/week/1/seasontype/3`
+        //const baseUrl = `http://cdn.espn.com/core/college-football/schedule?dates=${year}${parseInt(month) <= 9 ? "0" + parseInt(month) : parseInt(month)}${parseInt(day) <= 9 ? "0" + parseInt(day) : parseInt(day)}`;
+        const params = {
+            xhr: 1,
+        };
+
+        const res = await axios.get(baseUrl, {
+            params
+        });
+        return res.data.content.schedule;
+    },
     /**
      * Gets the College Football schedule data for a specified date if available.
      * @memberOf cfb
